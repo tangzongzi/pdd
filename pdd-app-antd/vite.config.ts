878dedbcb1bfd7d853926f20e7e808a4d7973882
tangzongzi <345581278@qq.com>
2025-04-13 21:52:04 +0800

    修复Netlify部署问题

--- pdd-app-antd/vite.config.ts
+++ pdd-app-antd/vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
@@ -26,5 +27,13 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          antd: ['antd', '@ant-design/icons'],
+        },
+      },
+    },
   },
 }); 
